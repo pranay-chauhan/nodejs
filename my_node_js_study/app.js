85c61d29dff9625836bc8a78edf5b8f1c4572226
pranay <pranay.s.chauhan@gmail.com>
2020-05-31 20:06:54 +0530

    basics in javascript using node js

--- my_node_js_study/app.js
+++
@@ -1,7 +0,0 @@
-console.log('Hello node js');
-
-var users = {
-    name:'Mohan',
-    age:30
-}
-console.log("The age of person is ",users)
---
+++ my_node_js_study/basic.js
@@ -0,0 +1,18 @@
+//Object 
+var users = {
+    name:'Mohan',
+    age:30
+}
+console.log("The age of person is ",users)
+
+//Annonymous function
+var show = function(){
+    console.log('Hello Annonymous function')
+}
+show()
+
+//Arrow function
+var user = (a)=>{
+    console.log(a)
+}
+user(10);
