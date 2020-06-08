b1259d0c5e455aa522e995ad2bb80f88468c0b9e
pranay <pranay.s.chauhan@gmail.com>
2020-06-08 14:51:29 +0530

    custom event added

---
+++ my_node_js_study/event.js
@@ -0,0 +1,12 @@
+//create and trigger the custom event
+const events = require('events');
+const event = new events.EventEmitter
+
+//event.on('click',(e)=>console.log(e));
+//event.emit('click','Hello I am learning node js here')
+// In the above case output will "Hello I am  learning node js code here"
+
+// we can pass number an argument
+event.on('click',(n1,n2)=>console.log(n1+n2))
+event.emit('click',4,10)
+//Note- make sure the first argument should be same in both event.on and event.emit
