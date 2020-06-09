81c32fce1a623d3870d9c0e905db578c859c5c7c
pranay <pranay.s.chauhan@gmail.com>
2020-06-09 12:06:01 +0530

    added the get and post method

--- expressjs/main.js
+++ expressjs/main.js
@@ -9,4 +9,8 @@ app.get('/',(req,res)=>{
 app.get('/users',(req,res)=>{
     res.send('users data acess')
 })
+app.post('/users/profiles',(req,res)=>{
+    res.send('users profile data acess')
+})
+//For post method open postman api tool and add the url there and select the POST method and click on  send you will get response which is in above console "users profiles data access"
 app.listen(8000,()=>console.log('Server created'))
